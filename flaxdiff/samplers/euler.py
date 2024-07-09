30ebed1ef456f6b00bc3c6c43c375d214d0aaa07
Ashish Kumar Singh <ashishkmr472@gmail.com>
2024-07-09 18:36:56 +0530

    feat: euler ancestral sampler

--- flaxdiff/samplers/euler.py
+++ flaxdiff/samplers/euler.py
@@ -33,3 +33,27 @@ class SimplifiedEulerSampler(DiffusionSampler):
         dx = (current_samples - reconstructed_samples) / current_sigma
         next_samples = current_samples + dx * dt
         return next_samples, state
+    
+class EulerAncestralSampler(DiffusionSampler):
+    """
+    Similar to EulerSampler but with ancestral sampling
+    """
+    def take_next_step(self, 
+                 current_samples, reconstructed_samples, 
+                 pred_noise, current_step, state:RandomMarkovState, next_step=None) -> tuple[jnp.ndarray, RandomMarkovState]:
+        current_alpha, current_sigma = self.noise_schedule.get_rates(current_step)
+        next_alpha, next_sigma = self.noise_schedule.get_rates(next_step)
+
+        sigma_up = (next_sigma**2 * (current_sigma**2 - next_sigma**2) / current_sigma**2) ** 0.5
+        sigma_down = (next_sigma**2 - sigma_up**2) ** 0.5
+        
+        dt = sigma_down - current_sigma
+        
+        x_0_coeff = (current_alpha * next_sigma - next_alpha * current_sigma) / (next_sigma - current_sigma)
+        dx = (current_samples - x_0_coeff * reconstructed_samples) / current_sigma
+        
+        state, subkey = state.get_random_key()
+        dW = jax.random.normal(subkey, current_samples.shape) * sigma_up
+        
+        next_samples = current_samples + dx * dt + dW
+        return next_samples, state
