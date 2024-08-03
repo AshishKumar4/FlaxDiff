b8fcdf6231c88c7f846b52539286e434d205ecac
Ashish Kumar Singh <ashishkmr472@gmail.com>
2024-08-03 18:30:38 +0000

    feat: dataset preparations

--- datasets/cc12m downloader.sh
+++ datasets/cc12m downloader.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-img2dataset --url_list ./datacache/cc12m.csv --input_format "csv"\
+img2dataset --url_list ./cc12m.tsv --input_format "tsv"\
          --url_col "image_url" --caption_col "caption" --output_format arrayrecord\
-           --output_folder gs://flaxdiff-datasets-regional/arrayrecord/cc12m --processes_count 64
+           --output_folder gs://flaxdiff-datasets-regional/arrayrecord/cc12m --processes_count 64\
  --thread_count 64 --image_size 256\
              --enable_wandb True --disallowed_header_directives '[]' --compute_hash None --max_shard_retry 3 --timeout 60
