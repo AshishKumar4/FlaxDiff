d2963ccf833f191147542669479db899cf61680f
Ashish Kumar Singh <ashishkmr472@gmail.com>
2024-08-08 13:24:19 +0000

    feat: synced changes

--- datasets/custom datasets downloader.sh
+++ datasets/custom datasets downloader.sh
@@ -3,7 +3,7 @@
 img2dataset --url_list $1 --input_format "parquet"\
          --url_col "url" --caption_col "caption" --output_format arrayrecord\
            --output_folder $2 --processes_count 64\
-          --thread_count 64 --image_size 256 --min_image_size 100 \
+          --thread_count 64 --image_size 256 --min_image_size 100 --number_sample_per_shard 40000 \
              --enable_wandb True --disallowed_header_directives '[]' --compute_hash None --max_shard_retry 3 --timeout 60
 
 # gs://flaxdiff-datasets-regional/arrayrecord/laion-aesthetics-12m+mscoco-2017
