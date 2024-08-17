c8c3ef938cd1d097875ab0e1353777df2c4ceccc
Ashish Kumar Singh <ashishkmr472@gmail.com>
2024-08-17 13:13:16 +0530

    feat: implementation of UViT

--- flaxdiff/models/simple_vit.py
+++ flaxdiff/models/simple_vit.py
@@ -3,9 +3,20 @@
 import jax
 import jax.numpy as jnp
 from flax import linen as nn
-from typing import Callable, Any
+from typing import Callable, Any, Optional, Tuple
 from .simple_unet import FourierEmbedding, TimeProjection, ConvLayer, kernel_init
 from .attention import TransformerBlock
+from flaxdiff.models.simple_unet import FourierEmbedding, TimeProjection, ConvLayer, kernel_init
+import einops
+from flax.typing import Dtype, PrecisionLike
+from functools import partial
+
+def unpatchify(x, channels=3):
+    patch_size = int((x.shape[2] // channels) ** 0.5)
+    h = w = int(x.shape[1] ** .5)
+    assert h * w == x.shape[1] and patch_size ** 2 * channels == x.shape[2], f"Invalid shape: {x.shape}, should be {h*w}, {patch_size**2*channels}"
+    x = einops.rearrange(x, 'B (h w) (p1 p2 C) -> B (h p1) (w p2) C', h=h, p1=patch_size, p2=patch_size)
+    return x
 
 class PatchEmbedding(nn.Module):
     patch_size: int
@@ -37,39 +48,28 @@ class PositionalEncoding(nn.Module):
                         (1, self.max_len, self.embedding_dim))
         return x + pe[:, :x.shape[1], :]
 
-class TransformerEncoder(nn.Module):
-    num_layers: int
-    num_heads: int
-    dropout_rate: float = 0.1
-    dtype: Any = jnp.float32
-    precision: Any = jax.lax.Precision.HIGH
-    use_projection: bool = False
-
-    @nn.compact
-    def __call__(self, x, context=None):
-        for _ in range(self.num_layers):
-            x = TransformerBlock(
-                heads=self.num_heads,
-                dim_head=x.shape[-1] // self.num_heads,
-                dropout_rate=self.dropout_rate,
-                dtype=self.dtype,
-                precision=self.precision,
-                use_self_and_cross=True,
-                use_projection=self.use_projection,
-            )(x, context)
-        return x
-
-class VisionTransformer(nn.Module):
+class UViT(nn.Module):
+    output_channels:int=3
     patch_size: int = 16
-    embedding_dim: int = 768
+    emb_features:int=768,
     num_layers: int = 12
     num_heads: int = 12
-    emb_features: int = 256
     dropout_rate: float = 0.1
     dtype: Any = jnp.float32
     precision: Any = jax.lax.Precision.HIGH
     use_projection: bool = False
+    activation:Callable = jax.nn.swish
+    norm_groups:int=8
+    dtype: Optional[Dtype] = None
+    precision: PrecisionLike = None
+    kernel_init: Callable = partial(kernel_init, 1.0)
 
+    def setup(self):
+        if self.norm_groups > 0:
+            self.norm = partial(nn.GroupNorm, self.norm_groups)
+        else:
+            self.norm = partial(nn.RMSNorm, 1e-5)
+            
     @nn.compact
     def __call__(self, x, temb, textcontext=None):
         # Time embedding
@@ -77,44 +77,65 @@ class VisionTransformer(nn.Module):
         temb = TimeProjection(features=self.emb_features)(temb)
 
         # Patch embedding
-        x = PatchEmbedding(patch_size=self.patch_size, embedding_dim=self.embedding_dim, 
+        x = PatchEmbedding(patch_size=self.patch_size, embedding_dim=self.emb_features, 
                            dtype=self.dtype, precision=self.precision)(x)
-        
-        # Add positional encoding
-        x = PositionalEncoding(max_len=x.shape[1], embedding_dim=self.embedding_dim)(x)
-        
         num_patches = x.shape[1]
-
+        
+        context_emb = nn.DenseGeneral(features=self.emb_features, kernel_init=self.kernel_init(), 
+                               dtype=self.dtype, precision=self.precision)(textcontext)
+        num_text_tokens = textcontext.shape[1]
+        
+        # print(f'Shape of x after patch embedding: {x.shape}, numPatches: {num_patches}, temb: {temb.shape}, context_emb: {context_emb.shape}')
+        
         # Add time embedding
         temb = jnp.expand_dims(temb, axis=1)
-        x = jnp.concatenate([x, temb], axis=1)
-
-        # Transformer encoder
-        x = TransformerEncoder(
-            num_layers=self.num_layers,
-            num_heads=self.num_heads,
-            dropout_rate=self.dropout_rate,
-            dtype=self.dtype,
-            precision=self.precision,
-            use_projection=self.use_projection
-        )(x, textcontext)
-
-        x = x[:, :num_patches, :]
-
-        # Reshape to image dimensions
-        batch, _, _ = x.shape
-        height = width = int((num_patches) ** 0.5)
-        x = jnp.reshape(x, (batch, height, width, self.embedding_dim))
-
-        # Final convolution to get the desired output channels
-        x = ConvLayer(
-            conv_type="conv",
-            features=3,
-            kernel_size=(3, 3),
-            strides=(1, 1),
-            kernel_init=kernel_init(0.0),
-            dtype=self.dtype,
-            precision=self.precision
-        )(x)
-
+        x = jnp.concatenate([x, temb, context_emb], axis=1)
+        # print(f'Shape of x after time embedding: {x.shape}')
+        
+        # Add positional encoding
+        x = PositionalEncoding(max_len=x.shape[1], embedding_dim=self.emb_features)(x)
+        
+        # print(f'Shape of x after positional encoding: {x.shape}')
+        
+        skips = []
+        # In blocks
+        for i in range(self.num_layers // 2):
+            x = TransformerBlock(heads=self.num_heads, dim_head=self.emb_features // self.num_heads, 
+                                 dtype=self.dtype, precision=self.precision, use_projection=self.use_projection, 
+                                 use_flash_attention=False, use_self_and_cross=False, force_fp32_for_softmax=True, 
+                                 only_pure_attention=False,
+                                 kernel_init=self.kernel_init())(x)
+            skips.append(x)
+            
+        # Middle block
+        x = TransformerBlock(heads=self.num_heads, dim_head=self.emb_features // self.num_heads, 
+                             dtype=self.dtype, precision=self.precision, use_projection=self.use_projection, 
+                             use_flash_attention=False, use_self_and_cross=True, force_fp32_for_softmax=True, 
+                             only_pure_attention=False,
+                             kernel_init=self.kernel_init())(x)
+        
+        # # Out blocks
+        for i in range(self.num_layers // 2):
+            skip = jnp.concatenate([x, skips.pop()], axis=-1)
+            skip = nn.DenseGeneral(features=self.emb_features, kernel_init=self.kernel_init(), 
+                                   dtype=self.dtype, precision=self.precision)(skip)
+            x = TransformerBlock(heads=self.num_heads, dim_head=self.emb_features // self.num_heads, 
+                                 dtype=self.dtype, precision=self.precision, use_projection=self.use_projection, 
+                                 use_flash_attention=False, use_self_and_cross=False, force_fp32_for_softmax=True, 
+                                 only_pure_attention=False,
+                                 kernel_init=self.kernel_init())(skip)
+        
+        # print(f'Shape of x after transformer blocks: {x.shape}')
+        x = self.norm()(x)
+        
+        # print(f'Shape of x after norm: {x.shape}')
+        
+        patch_dim = self.patch_size ** 2 * self.output_channels
+        x = nn.Dense(features=patch_dim, dtype=self.dtype, precision=self.precision, kernel_init=self.kernel_init())(x)
+        # print(f'Shape of x after patch dense layer: {x.shape}, patch_dim: {patch_dim}')
+        x = x[:, 1 + num_text_tokens:, :]
+        x = unpatchify(x, channels=self.output_channels)
+        # print(f'Shape of x after final dense layer: {x.shape}')
+        x = nn.Dense(features=self.output_channels, dtype=self.dtype, precision=self.precision, kernel_init=self.kernel_init())(x)
+        
         return x
