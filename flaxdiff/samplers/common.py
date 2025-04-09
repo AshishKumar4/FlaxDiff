2e7035bba110110258d5b993c742c22910f2330f
Ashish Kumar Singh <ashishkmr472@gmail.com>
2025-04-09 19:09:37 +0000

    feat: trying to make shaping consistent across schedulers

--- flaxdiff/samplers/common.py
+++ flaxdiff/samplers/common.py
@@ -67,7 +67,7 @@ class DiffusionSampler():
     # Used to sample from the diffusion model
     def sample_step(self, sample_model_fn, current_samples:jnp.ndarray, current_step, model_conditioning_inputs, next_step=None, state:MarkovState=None) -> tuple[jnp.ndarray, MarkovState]:
         # First clip the noisy images
-        step_ones = jnp.ones((current_samples.shape[0], ), dtype=jnp.int32)
+        step_ones = jnp.ones((len(current_samples), ), dtype=jnp.int32)
         current_step = step_ones * current_step
         next_step = step_ones * next_step
         pred_images, pred_noise, _ = sample_model_fn(current_samples, current_step, *model_conditioning_inputs)
